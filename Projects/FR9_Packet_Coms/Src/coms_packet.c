d336b61b3e629fe78ce3d62691f4ceca0530751c
Nick Masson <nerdineering@gmail.com>
2017-11-13 21:00:29 +0000

    update to packet coms code

--- Projects/FR9_Packet_Coms/Src/coms_packet.c
+++ Projects/FR9_Packet_Coms/Src/coms_packet.c
@@ -26,65 +26,34 @@
 #define PACKET_MAX_PL_LEN		1024
 
 /* Coms port */
-#define COMS_PORT_NUMBER 				( ( uint16_t ) 7001 )
+#define COMS_PORT_NUMBER 		7001
 
-#define COMM_FW_VERSION 0x1
+#define READ_IO32( addr ) (*(volatile int*)(addr))
+
+//#define COMM_FW_VERSION 0x1
 #define FW_VERSION_MAJOR 0x2
 #define FW_VERSION_MINOR 0x3
 
 
 // Private variables
 static uint8_t send_buffer[PACKET_MAX_PL_LEN];
+static uint8_t RxBuffer[PACKET_MAX_PL_LEN];
+static uint16_t RxState = 0;
+static uint16_t RxTimer = 0;
 //static SemaphoreHandle_t xComs_TX_Semaphore = NULL;
 
-/* Standard TFTP opcodes. */
-typedef enum
-{
-	eReadRequest = 1,
-	eWriteRequest,
-	eData,
-	eAck,
-	eError
-} eTFTPOpcode_t;
 
-
-
-/* The index for the error string below MUST match the value of the applicable
-eTFTPErrorCode_t error code value. */
-static const char *cErrorStrings[] =
-{
-	NULL, /* Not valid. */
-	"File not found.",
-	"Access violation.",
-	"Disk full or allocation exceeded.",
-	"Illegal TFTP operation.",
-	"Unknown transfer ID.",
-	"File already exists.",
-	"No such user."
-};
-
-/* Error codes from the RFC. */
-typedef enum
-{
-	eFileNotFound = 1,
-	eAccessViolation,
-	eDiskFull,
-	eIllegalTFTPOperation,
-	eUnknownTransferID,
-	eFileAlreadyExists
-} eTFTPErrorCode_t;
-
-
-static void coms_packet_tx(Socket_t xSocket, struct freertos_sockaddr *pxClient, eTFTPErrorCode_t eErrorCode);
+static void coms_packet_tx(Socket_t xSocket, struct freertos_sockaddr *pxClient, uint16_t data_length);
 static void coms_packet_process_rx(uint8_t *data, int32_t len, Socket_t xSocket, struct freertos_sockaddr *pxClient);
-
 static void coms_packet_rx_task( void *pvParameters );
-
+static void coms_packet_timeout_task( void *pvParameters );
+static uint32_t buffer_get_uint32(const uint8_t *buffer, int32_t *index);
+static void buffer_append_uint32(uint8_t* buffer, uint32_t number, int32_t *index);
 
 void coms_packet_create_task(void)
 {
 	xTaskCreate(coms_packet_rx_task, "coms_rx_task", configMINIMAL_STACK_SIZE*2, NULL, 1, ( TaskHandle_t * )NULL);
-
+	xTaskCreate(coms_packet_timeout_task, "coms_time_out", configMINIMAL_STACK_SIZE, NULL, 1, ( TaskHandle_t * )NULL);
 }
 
 /**
@@ -92,52 +61,29 @@ void coms_packet_create_task(void)
  *
  *
  */
-static void coms_packet_tx(Socket_t xSocket, struct freertos_sockaddr *pxClient, eTFTPErrorCode_t eErrorCode)
+static void coms_packet_tx(Socket_t xSocket, struct freertos_sockaddr *pxClient, uint16_t data_length)
 {
 uint8_t *pucUDPPayloadBuffer = NULL;
-const size_t xFixedSizePart = ( size_t ) 5; /* 2 byte opcode, plus two byte error code, plus string terminating 0. */
-const size_t xNumberOfErrorStrings = sizeof( cErrorStrings ) / sizeof( char * );
-size_t xErrorCode = ( size_t ) eErrorCode, xTotalLength = 0; /* Only initialised to keep compiler quiet. */
-const char *pcErrorString = NULL;
 int32_t lReturned;
 
-	/* The total size of the packet to be sent depends on the length of the
-	error string. */
-	if( xErrorCode < xNumberOfErrorStrings )
-	{
-		pcErrorString = cErrorStrings[ xErrorCode ];
-
-		/* This task is going to send using the zero copy interface.  The data
-		being sent is therefore written directly into a buffer that is passed
-		into, rather than copied into, the FreeRTOS_sendto() function.  First
-		obtain a buffer of adequate length from the IP stack into which the
-		error packet will be written.  Although a max delay is used, the actual
-		delay will be capped to ipconfigMAX_SEND_BLOCK_TIME_TICKS. */
-		xTotalLength = strlen( pcErrorString ) + xFixedSizePart;
-		pucUDPPayloadBuffer = ( uint8_t * ) FreeRTOS_GetUDPPayloadBuffer( xTotalLength, portMAX_DELAY );
-	}
+	/* This task is going to send using the zero copy interface.  The data
+	being sent is therefore written directly into a buffer that is passed
+	into, rather than copied into, the FreeRTOS_sendto() function.  First
+	obtain a buffer of adequate length from the IP stack into which the
+	error packet will be written.  Although a max delay is used, the actual
+	delay will be capped to ipconfigMAX_SEND_BLOCK_TIME_TICKS. */
+	pucUDPPayloadBuffer = ( uint8_t * ) FreeRTOS_GetUDPPayloadBuffer( data_length, portMAX_DELAY );
 
 	if( pucUDPPayloadBuffer != NULL )
 	{
-		FreeRTOS_printf( ( "Error: %s\n", pcErrorString ) );
 
-		/* Create error packet: Opcode. */
-		pucUDPPayloadBuffer[ 0 ] = 0;
-		pucUDPPayloadBuffer[ 1 ] = ( uint8_t ) eError;
-
-		/* Create error packet: Error code. */
-		pucUDPPayloadBuffer[ 2 ] = 0;
-		pucUDPPayloadBuffer[ 3 ] = ( uint8_t ) eErrorCode;
-
-		/* Create error packet: Error string. */
-		strcpy( ( ( char * ) &( pucUDPPayloadBuffer[ 4 ] ) ), pcErrorString );
-
+		memcpy( pucUDPPayloadBuffer, send_buffer, data_length); // THIS SEEMS REDUNDANT. WHY COPY WHEN ITS SUPPOSED TO BE "ZERO COPY"??
 		/* Pass the buffer into the send function.  ulFlags has the
 		FREERTOS_ZERO_COPY bit set so the IP stack will take control of the
 		buffer rather than copy data out of the buffer. */
 		lReturned = FreeRTOS_sendto( xSocket,  						/* The socket to which the error frame is sent. */
-									( void * ) pucUDPPayloadBuffer, /* A pointer to the the data being sent. */
-									xTotalLength, 					/* The length of the data being sent. */
+									( const void * )pucUDPPayloadBuffer, /* A pointer to the the data being sent. */
+									data_length, 					/* The length of the data being sent. */
 									FREERTOS_ZERO_COPY, 			/* ulFlags with the FREERTOS_ZERO_COPY bit set. */
 									pxClient, 			/* Where the data is being sent. */
 									sizeof( *pxClient ) );
@@ -159,6 +105,22 @@ int32_t lReturned;
 	}
 }
 
+//move to helper function
+static uint32_t buffer_get_uint32(const uint8_t *buffer, int32_t *index) {
+	uint32_t res =	((uint32_t) buffer[*index]) << 24 |
+					((uint32_t) buffer[*index + 1]) << 16 |
+					((uint32_t) buffer[*index + 2]) << 8 |
+					((uint32_t) buffer[*index + 3]);
+	*index += 4;
+	return res;
+}
+
+static void buffer_append_uint32(uint8_t* buffer, uint32_t number, int32_t *index) {
+	buffer[(*index)++] = number >> 24;
+	buffer[(*index)++] = number >> 16;
+	buffer[(*index)++] = number >> 8;
+	buffer[(*index)++] = number;
+}
 
 
 /**
@@ -170,14 +132,12 @@ int32_t lReturned;
  * @param len
  * The length of the buffer.
  */
-static void coms_packet_process_rx(uint8_t *data, int32_t len, Socket_t xSocket, struct freertos_sockaddr *pxClient) {
-	if (!len) {
-		return;
-	}
+static void coms_packet_process_rx(uint8_t *data, int32_t len, Socket_t xSocket, struct freertos_sockaddr *pxClient){
 
 	COMM_PACKET_ID packet_id;
 	int32_t ind = 0;
-
+	volatile uint32_t address_32 = 0;
+	uint32_t result = 0;
 
 	packet_id = data[0];
 	data++;
@@ -189,7 +149,21 @@ static void coms_packet_process_rx(uint8_t *data, int32_t len, Socket_t xSocket,
 		send_buffer[ind++] = COMM_FW_VERSION;
 		send_buffer[ind++] = FW_VERSION_MAJOR;
 		send_buffer[ind++] = FW_VERSION_MINOR;
-		coms_packet_tx(xSocket,pxClient, eIllegalTFTPOperation);
+		coms_packet_tx(xSocket,pxClient, ind);
+		break;
+
+	case COMM_READ_REG32:
+		ind = 0;
+		address_32 = buffer_get_uint32(data, &ind);
+
+		//read register
+		result = READ_IO32(address_32);
+
+		ind = 0;
+		send_buffer[ind++] = COMM_READ_REG32;
+
+		buffer_append_uint32(send_buffer, result, &ind);
+		coms_packet_tx(xSocket,pxClient,ind);
 		break;
 
 
@@ -206,6 +180,10 @@ static void coms_packet_rx_task( void *pvParameters )
 	struct freertos_sockaddr xClient, xBindAddress;
 	uint32_t xClientLength = sizeof( xClient ), ulIPAddress;
 	Socket_t xComPortListeningSocket;
+
+
+	uint32_t PayloadLength;
+	uint32_t RxDataPtr;
 
 	/*just to remove compiler warning*/
 	(void) pvParameters;
@@ -225,8 +203,6 @@ static void coms_packet_rx_task( void *pvParameters )
 			configUDP_LOGGING_ADDR3 );
 	xBindAddress.sin_port = FreeRTOS_htons( COMS_PORT_NUMBER );
 	FreeRTOS_bind( xComPortListeningSocket, &xBindAddress, sizeof( xBindAddress ) );
-
-
 
    for( ;; )
    {
@@ -250,9 +226,77 @@ static void coms_packet_rx_task( void *pvParameters )
        {
            /* Data was received and can be processed here. */
     	   SEGGER_SYSVIEW_Print("got some data COMS");
-    	   coms_packet_process_rx(pucUDPPayloadBuffer, lBytes, xComPortListeningSocket, &xClient);
-    	   FreeRTOS_ReleaseUDPPayloadBuffer( ( void * ) pucUDPPayloadBuffer );
+
+    		unsigned char rx_data;
+    		const int rx_timeout = 50;
+
+    		for (int i = 0; i < lBytes; i++){
+
+    			rx_data = pucUDPPayloadBuffer[i];
+
+    			switch(RxState){
+    			case 0:
+    				if (rx_data == 2){
+
+    				}
+    				else {
+    					RxState = 0;
+    				}
+    				break;
+    			case 1:
+    	            PayloadLength = (unsigned int)rx_data << 8;
+    	            RxState++;
+    	            RxTimer = rx_timeout;
+    	            RxDataPtr = 0;
+    	            PayloadLength = 0;
+    	            break;
+    			}
+    	        case 2:
+    	            PayloadLength |= (unsigned int)rx_data;
+    	            if (PayloadLength <= PACKET_MAX_PL_LEN && PayloadLength > 0) {
+    	                RxState++;
+    	                RxTimer = rx_timeout;
+    	            } else {
+    	                RxState = 0;
+    	            }
+    	            break;
+    	        case 3:
+    	            RxBuffer[RxDataPtr++] = rx_data;
+    	            if (RxDataPtr == PayloadLength) {
+    	                RxState++;
+    	            }
+    	            RxTimer = rx_timeout;
+    	            break;
+    	        case 4:
+    	        	coms_packet_process_rx(RxBuffer, PayloadLength, xComPortListeningSocket, &xClient);
+    	            RxState = 0;
+    	            break;
+
+    	        default:
+    	            RxState = 0;
+    	            break;
+    		}
+
+    		FreeRTOS_ReleaseUDPPayloadBuffer( ( void * ) pucUDPPayloadBuffer );
        }
 
    }
 }
+
+static void coms_packet_timeout_task( void *pvParameters )
+{
+	/*just to remove compiler warning*/
+	(void) pvParameters;
+	if (0 < RxTimer){
+		RxTimer--;
+
+	}
+	else{
+		RxState = 0;
+		RxTimer = 0;
+	}
+	vTaskDelay(10);
+}
+
+
+
